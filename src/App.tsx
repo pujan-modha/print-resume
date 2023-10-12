419c271c044e835472a0d0de0b8b72af8f2cb51b
Pujan Modha <pujanmodha19@gmail.com>
2023-10-12 17:56:45 +0530

    Responsive fixes

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import ExperiencePrint from "./components/ExperiencePrint.tsx";
 import ProjectsPrint from "./components/ProjectsPrint.tsx";
 import CertificationsPrint from "./components/CertificationsPrint.tsx";
 import AchievementsPrint from "./components/AchievementsPrint.tsx";
+import MediaQuery from "react-responsive";
 
 function Print() {
     const [title] = useState(HeaderFullName);
@@ -18,30 +19,41 @@ function Print() {
     });
     return (
         <>
-            <div className='mx-[2.5%] md:mx-auto py-[2.5%] print:py-0 md:bg-fixed' id='bg-pattern'>
-                <div className='text-foreground bg-background font-mono box-border max-w-3xl mx-auto print:border-none border-4 border-foreground w-full'>
-                    <HeaderPrint/>
-                    <div className='px-[1%]'>
-                        <HorizontalRulePrint/>
-                        <ContactPrint/>
-                        <HorizontalRulePrint/>
-                        <EducationPrint/>
-                        <HorizontalRulePrint/>
-                        <SkillsPrint/>
-                        <HorizontalRulePrint/>
-                        <ExperiencePrint/>
-                        <HorizontalRulePrint/>
-                        <ProjectsPrint/>
-                        <HorizontalRulePrint/>
-                        <div className='md:grid md:grid-cols-2'>
-                            <CertificationsPrint/>
-                            <div className='md:hidden md:scale-0'>
-                                <HorizontalRulePrint/>
-                            </div>
-                            <AchievementsPrint/>
+            <div className='px-[2.5%] md:mx-auto py-[2.5%] print:py-0 md:bg-fixed' id='bg-pattern'>
+                <div className='h-screen md:h-full text-foreground bg-background font-mono box-border max-w-3xl mx-auto print:border-none border-4 border-foreground w-full'>
+                    <MediaQuery maxWidth={768}>
+                        <div className='print:hidden p-[2.5%]'>
+                            <p className='text-lg font-semibold'>
+                                Kindly open this website on device with bigger screen or Switch to Desktop mode to view this website.
+                            </p>
                         </div>
-                        <hr className='mx-auto border-2 border-opacity-50 border-primary my-[1%]'/>
-                    </div>
+                    </MediaQuery>
+                    <MediaQuery minWidth={768}>
+                        <div>
+                            <HeaderPrint/>
+                            <div className='px-[1%]'>
+                                <HorizontalRulePrint/>
+                                <ContactPrint/>
+                                <HorizontalRulePrint/>
+                                <EducationPrint/>
+                                <HorizontalRulePrint/>
+                                <SkillsPrint/>
+                                <HorizontalRulePrint/>
+                                <ExperiencePrint/>
+                                <HorizontalRulePrint/>
+                                <ProjectsPrint/>
+                                <HorizontalRulePrint/>
+                                <div className='md:grid md:grid-cols-2'>
+                                    <CertificationsPrint/>
+                                    <div className='md:hidden md:scale-0'>
+                                        <HorizontalRulePrint/>
+                                    </div>
+                                    <AchievementsPrint/>
+                                </div>
+                                <hr className='mx-auto border-2 border-opacity-50 border-primary my-[1%]'/>
+                            </div>
+                        </div>
+                    </MediaQuery>
                 </div>
             </div>
         </>
